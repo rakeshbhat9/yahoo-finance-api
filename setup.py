064424518b7571cb37745b85d073190905979909
Rakesh <rakeshbhat9@gmail.com>
2021-03-03 22:51:07 +0000

    Updated package in pypi to v0.6

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name = 'yahoo-finance-api',
-    packages = ['yahoo_finance'],
-    version = 'v0.1', 
+    version = 'v0.6', 
     description = 'Package to source data from Yahoo Finance',
     author = 'Rakesh Bhat',
     author_email = 'rakeshbhat9@gmail.com',
     url = 'https://github.com/rakeshbhat9/yahoo-finance-api',
-    download_url = 'https://github.com/rakeshbhat9/yahoo-finance-api/archive/v0.1.tar.gz',
-    keywords = ['v0.1'],
+    download_url = 'https://github.com/rakeshbhat9/yahoo-finance-api/archive/v0.2.tar.gz',
+    keywords = ['v0.6'],
     classifiers = [],
-        install_requires=[
-        'pandas==1.1.3',
-        'requests==2.24.0',
-        'bs4==4.9.3'
-        'matplotlib==3.3.2']
+    packages=find_packages(),
+    install_requires=[
+    'pandas==1.1.3',
+    'requests==2.24.0',
+    'beautifulsoup4==4.9.3',
+    'matplotlib==3.3.2']
 )
+# python setup.py sdist upload -r pypi
