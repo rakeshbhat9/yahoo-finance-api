4a47cf93669479fe53d791c3d205145f0a113c9a
Rakesh <rakeshbhat9@gmail.com>
2021-03-03 22:20:04 +0000

    Added install requires to setup

--- setup.py
+++ setup.py
@@ -11,4 +11,9 @@ setup(
     download_url = 'https://github.com/rakeshbhat9/yahoo-finance-api/archive/v0.1.tar.gz',
     keywords = ['v0.1'],
     classifiers = [],
+        install_requires=[
+        'pandas==1.1.3',
+        'requests==2.24.0',
+        'bs4==4.9.3'
+        'matplotlib==3.3.2']
 )
